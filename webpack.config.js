3d26de778f65c58ffc06c5ec1119603ee8657074
Flowers Safety <40meta@gmail.com>
2016-11-19 18:22:48 +0600

    css "compilation"

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
-var path = require('path')
+var path = require('path'),
+    ExtractTextPlugin = require("extract-text-webpack-plugin");
 
 module.exports = {
     debug: true,
@@ -8,11 +9,17 @@ module.exports = {
         //order_pug: "pug-loader!./pug/pages/order.pug",
         //product_list_pug: "pug-loader!./pug/pages/product_list.pug",
         product_list: "./js/pages/product_list.js",
-        order: "./js/pages/order.js"
+        order: "./js/pages/order.js",
+        // копирование css в build
+        resourses: "./resources.js"
     },
     output: {
         path: path.join(__dirname, 'build', 'js'),
-        filename: '[name].js'
+        filename: '[name].js',
+        // не работает; не исключает генерацию сурсмапов
+        devtoolLineToLine: {
+          exclude: /css/
+        }
     },
     module: {
         loaders: [
@@ -23,9 +30,18 @@ module.exports = {
             query: {
               presets: ['react', 'es2015']
             }
+          },
+          //https://github.com/webpack/extract-text-webpack-plugin/blob/webpack-1/README.md
+          {
+            test: /\.css$/,
+            loader: ExtractTextPlugin.extract("style-loader", "css-loader")
           }
         ],
     },
+    plugins: [
+      new ExtractTextPlugin("../css/styles.css")
+      //new ExtractTextPlugin("[name].css")
+    ],
     resolve: {
         extensions: [
             "",
