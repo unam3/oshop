883639242e14aa43e62d89086dfff4915eb0e8d0
Flowers Safety <40meta@gmail.com>
2016-11-21 19:29:13 +0600

    product_list: added product style fix

--- src/js/pages/product_list.js
+++ src/js/pages/product_list.js
@@ -77,18 +77,22 @@ const React = require("react"),
     Product = ({brand, name, cost, cart, id, onAddToCart}) => (
       <div className="product product-list__product flex-column">
         <img className="product__preview product-list__product-preview" />
-        <div className="product-list__product-link product-list_lmargin">
+        <div className="product-list__product-link product-list__product_margin">
           <a href="#" className="blue-text">
             {brand} {name}
           </a>
         </div>
-        <div className="product-list_lmargin flex-row">
+        <div className="product-list__product_margin flex-row">
           <div className="product__cost product-list-cost">{cost} руб.</div>
           {
-            !cart[id] && <BlueButton
-              additionalClasses="add-to-cart" text="В корзину"
-              fobj={{f: onAddToCart, args: {id: id}}}
-              />
+            cart[id] ?
+              <div className="product-in-cart">
+                В корзине
+              </div>
+              : <BlueButton
+                additionalClasses="add-to-cart" text="В корзину"
+                fobj={{f: onAddToCart, args: {id: id}}}
+                />
           }
         </div>
       </div>
